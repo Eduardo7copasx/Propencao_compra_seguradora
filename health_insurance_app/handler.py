742869f46287df68242c77fd5eb4593c72eb1d89
Eduardo7copasx <eduardopessoal539@outlook.com>
2026-05-06 07:00:53 -0300

    Finalização das analises do projeto

--- health_insurance_app/handler.py
+++ health_insurance_app/handler.py
@@ -1,46 +1,56 @@
 import pickle
+from pathlib import Path
+
 import pandas as pd
 from flask import Flask, request, Response
-from healthinsurance.Healthinsurance import Healthinsurance
 
-#loading model
+from healthinsurance.Healthinsurance import HealthInsurance
 
-model = pickle.load(open('src/models/model_health_insurance.pkl','rb'))
 
-#inicialização
+BASE_DIR = Path(__file__).resolve().parent
+MODEL_PATH = BASE_DIR / "models" / "model_health_insurance.pkl"
+
+with open(MODEL_PATH, "rb") as file:
+    model = pickle.load(file)
+
 app = Flask(__name__)
 
-@app.route('/predict', methods=['POST'])
+
+@app.route("/", methods=["GET"])
+def home():
+    return Response('{"status": "ok"}', status=200, mimetype="application/json")
+
+
+@app.route("/health", methods=["GET"])
+def health():
+    return Response('{"status": "healthy"}', status=200, mimetype="application/json")
+
+
+@app.route("/predict", methods=["POST"])
 def health_insurance_predict():
     test_json = request.get_json()
 
-    if test_json: # there is data
-        if isinstance(test_json, dict): # unique example
-            test_raw = pd.DataFrame(test_json, index=[0])
+    if not test_json:
+        return Response("{}", status=200, mimetype="application/json")
 
-        else: # multiple example
-            test_raw = pd.DataFrame(test_json, columns=test_json[0].keys())
-
-        # Instantiate Rossmann class
-        pipeline = HealthInsurance()
-
-        # data cleaning
-        df1 = pipeline.data_cleaning(test_raw)
-
-        # feature engineering
-        df2 = pipeline.feature_engineering(df1)
-
-        # data preparation
-        df3 = pipeline.data_preparation(df2)
-
-        # prediction
-        df_response = pipeline.get_prediction(model, test_raw, df3)
-
-        return df_response
-
+    if isinstance(test_json, dict):
+        test_raw = pd.DataFrame(test_json, index=[0])
     else:
-        return Response('{}', status=200, mimetype='application/json')
+        test_raw = pd.DataFrame(test_json, columns=test_json[0].keys())
+
+    pipeline = HealthInsurance()
+
+    df1 = pipeline.data_cleaning(test_raw)
+    df2 = pipeline.feature_engineering(df1)
+    df3 = pipeline.data_preparation(df2)
+
+    df_response = pipeline.get_prediction(model, test_raw, df3)
+
+    return Response(df_response, status=200, mimetype="application/json")
 
 
-if __name__ == '__main__':
-    app.run('0.0.0.0', debug=True)
+if __name__ == "__main__":
+    import os
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
--- health_insurance_app/healthinsurance/Healthinsurance.py
+++ health_insurance_app/healthinsurance/Healthinsurance.py
@@ -1,70 +1,86 @@
 import pickle
+from pathlib import Path
+
 import pandas as pd
 import numpy as np
 
-class Healthinsurance:
+
+class HealthInsurance:
     def __init__(self):
-        self.home_path = '/home/ds-eduardo/propensao_classificacao/'
-        self.annual_premium_scaler =                 pickle.load(open(self.home_path + 'src/features/annual_premium_scaler.pkl'))
-        self.age_scaler =                            pickle.load(open(self.home_path + 'src/features/age_scaler.pkl'))
-        self.vintage_scaler =                        pickle.load(open(self.home_path + 'src/features/vintage_scaler.pkl'))
-        self.target_encode_gender_scaler =           pickle.load(open(self.home_path + 'src/features/target_encode_gender_scaler.pkl'))
-        self.target_encode_region_code_scaler =      pickle.load(open(self.home_path + 'src/features/target_encode_region_code_scaler.pkl'))
-        self.fe_policy_sales_channel_scaler =        pickle.load(open(self.home_path + 'src/features/fe_policy_sales_channel_scaler.pkl'))
-    
-    def data_cleaning(df1):
+        self.home_path = Path(__file__).resolve().parents[1]
 
-        #rename coloumns
-        df_raw.columns = df_raw.columns.str.lower()
+        with open(self.home_path / "features" / "annual_premium_scaler.pkl", "rb") as file:
+            self.annual_premium_scaler = pickle.load(file)
 
-        #rename
-        df1 = df_raw
+        with open(self.home_path / "features" / "age_scaler.pkl", "rb") as file:
+            self.age_scaler = pickle.load(file)
+
+        with open(self.home_path / "features" / "vintage_scaler.pkl", "rb") as file:
+            self.vintage_scaler = pickle.load(file)
+
+        with open(self.home_path / "features" / "target_encode_gender_scaler.pkl", "rb") as file:
+            self.target_encode_gender_scaler = pickle.load(file)
+
+        with open(self.home_path / "features" / "target_encode_region_code_scaler.pkl", "rb") as file:
+            self.target_encode_region_code_scaler = pickle.load(file)
+
+        with open(self.home_path / "features" / "fe_policy_sales_channel_scaler.pkl", "rb") as file:
+            self.fe_policy_sales_channel_scaler = pickle.load(file)
+
+    def data_cleaning(self, df1):
+        df1 = df1.copy()
+        df1.columns = df1.columns.str.lower()
         return df1
 
-    def feature_engineering(df2):
+    def feature_engineering(self, df2):
+        df2 = df2.copy()
 
-        # vehicle age
-        df2['vehicle_age'] = df2['vehicle_age'].apply(lambda x:'over_2_years' if x== '> 2 Years' else 'between_1_2_years' if x =='1-2 Year' else 'below_1_year')
-        # vehicle damage
-        df2['vehicle_damage'] = df2['vehicle_damage'].apply(lambda x:1 if x =='Yes' else 0)
+        df2["vehicle_age"] = df2["vehicle_age"].apply(
+            lambda x: "over_2_years"
+            if x == "> 2 Years"
+            else "between_1_2_years"
+            if x == "1-2 Year"
+            else "below_1_year"
+        )
+
+        df2["vehicle_damage"] = df2["vehicle_damage"].apply(
+            lambda x: 1 if x == "Yes" else 0
+        )
 
         return df2
 
+    def data_preparation(self, df5):
+        df5 = df5.copy()
 
-    def data_preparation(df5):
-
-        #annual_premium
         df5["annual_premium"] = self.annual_premium_scaler.transform(df5[["annual_premium"]])
-
-        #age
         df5["age"] = self.age_scaler.transform(df5[["age"]])
-
-        #vintage
         df5["vintage"] = self.vintage_scaler.transform(df5[["vintage"]])
 
-        ## 5.3 Encoder
-        # gender - One Hot Encoding / Target Encoding
-        df5['gender'] = df5['gender'].map( self.target_encode_gender_scaler ).astype(float)
+        df5["gender"] = df5["gender"].map(self.target_encode_gender_scaler).astype(float)
+        df5.loc[:, "region_code"] = df5["region_code"].map(self.target_encode_region_code_scaler)
 
-        # region_code - Target Encoding / Frequency Encoding
-        df5.loc[:, 'region_code'] = df5['region_code'].map( self.target_encode_region_code_scaler )
+        df5 = pd.get_dummies(df5, prefix="vehicle_age", columns=["vehicle_age"])
 
-        # vehicle_age - One Hot Encoding / Frequency Encoding
-        df5 = pd.get_dummies( df5, prefix='vehicle_age', columns=['vehicle_age'] )
+        df5.loc[:, "policy_sales_channel"] = df5["policy_sales_channel"].map(
+            self.fe_policy_sales_channel_scaler
+        )
 
-        # policy_sales_channel - Target Encoding / Frequency Encoding
-        df5.loc[:, 'policy_sales_channel'] = df5['policy_sales_channel'].map( self.fe_policy_sales_channel_scaler )
-
-        col_selected = ['vintage', 'annual_premium', 'age', 'region_code', 'vehicle_damage', 'policy_sales_channel', 'previously_insured']
+        col_selected = [
+            "vintage",
+            "annual_premium",
+            "age",
+            "region_code",
+            "vehicle_damage",
+            "policy_sales_channel",
+            "previously_insured",
+        ]
 
         return df5[col_selected]
-    
-    def get_predict(self, model, original_data,test_data):
-        #model predict
+
+    def get_prediction(self, model, original_data, test_data):
         pred = model.predict_proba(test_data)
 
-        original_data['score'] = pred
+        original_data = original_data.copy()
+        original_data["score"] = pred[:, 1]
 
-        return original_data.to_json(orient='records', date_format='iso')
-
-    
+        return original_data.to_json(orient="records", date_format="iso")
